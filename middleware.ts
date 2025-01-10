03b9b9c9b937986ac8a423e761799a1131afe4e1
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-01-10 14:00:58 -0300

    changed route of the redirect and removed log

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,9 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET! });
-  console.log(token?.role, req.url, "HOLAAA");
 
   if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/unauthorized", req.url));
   }
 
   // Verificar si el usuario tiene el rol de ADMIN
