c4bf96b8b3aea078bb56a65a5405c77ab01fa05a
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-01-10 14:01:44 -0300

    updated styles of the button

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -9,7 +9,9 @@ export default function SignIn() {
                 await signIn("github", { redirectTo: "/tasks" })
             }}
         >
-            <button type="submit">Signin with GitHub</button>
+            <button type="submit" className="w-full bg-purple-600 dark:bg-purple-700 px-3 text-white py-2 rounded-md text-center hover:bg-purple-700 dark:hover:bg-purple-800">
+                Signin with GitHub
+            </button>
         </form>
     )
 } 
