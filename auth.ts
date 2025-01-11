c820619a5155a42ba37a4cff4e7cefab1aacbed0
Pablociarrocca <pablorumualdo2305@gmail.com>
2025-01-11 09:30:13 -0300

    Update auth.ts
    added the credentials provider so the user can choose to create an account

--- auth.ts
+++ auth.ts
@@ -1,9 +1,59 @@
 import NextAuth from "next-auth";
+
 import GitHub from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 import { prisma } from "@/lib/prisma";
 
+import { compare } from "bcryptjs";
+
+import { User } from "@prisma/client";
+
+type UserWithPassword = User & { password: string };
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: { email: string; password: string }) {
+        if (!credentials) return null;
+
+        const { email, password } = credentials;
+
+        // Buscar el usuario en la base de datos
+        const user = (await prisma.user.findUnique({
+          where: { email },
+        })) as UserWithPassword | null;
+
+        if (!user) {
+          throw new Error("Invalid email or password");
+        }
+
+        // Validar la contraseña
+        const isValidPassword = await compare(password, user.password);
+        if (!isValidPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        // Retornar los datos del usuario
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
   callbacks: {
     async signIn({ user }) {
       const email = user.email!;
@@ -31,5 +81,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
       return token;
     },
+  },
+  secret: process.env.AUTH_SECRET!,
+  session: {
+    strategy: "jwt", // Usamos JWT para manejar sesiones
+  },
+  pages: {
+    signIn: "/auth/login", // Página personalizada de inicio de sesión
+    error: "/auth/unauthorized", // Página personalizada de error
   },
 });
